b6d40d7e1118913eef029e4d7ed3a3c1640cea74
Jose K James <josekjames5@gmail.com>
2023-03-31 23:30:03 +0530

    init_app

---
+++ spec_assist_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: HomePage(),
+        theme: ThemeData(
+          primaryColor: Colors.deepPurpleAccent,
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: AppBarTheme(backgroundColor: Colors.deepPurpleAccent),
+          textTheme: TextTheme(
+            displayLarge: TextStyle(color: Colors.black),
+          ),
+          colorScheme: ColorScheme.fromSwatch()
+              .copyWith(secondary: Colors.deepPurpleAccent)
+              .copyWith(secondary: Colors.blue),
+        ),
+      ),
+    );
